1121558aa94eb8bca3777840ca1512d2ccd60e3e
bulong0721 <bulong0721@163.com>
2017-05-01 21:19:53 +0800

    0501

--- package/mytms-server/src/main/java/org/mytms/web/controller/ClientController.java
+++ package/mytms-server/src/main/java/org/mytms/web/controller/ClientController.java
@@ -1,5 +1,6 @@
 package org.mytms.web.controller;
 
+import com.google.common.collect.Lists;
 import org.mytms.common.ajax.AjaxPageableResponse;
 import org.mytms.common.domain.Client;
 import org.mytms.common.service.ClientService;
@@ -40,4 +41,67 @@ public class ClientController {
         return response;
     }
 
+    @ResponseBody
+    @RequestMapping("/site/paging")
+    public AjaxPageableResponse listSite() {
+        List<Site> all = Lists.newArrayList();
+        Site site = new Site(1, "web01", "192.168.0.1", "网易", "www.163.com", 200);
+        site.steps = Lists.newArrayList();
+        site.steps.add(new Step(1, "step01", "www.163.com/music", "", 3000, "结果01", 200));
+        site.steps.add(new Step(2, "step02", "www.163.com/games", "", 3000, "结果02", 200));
+        all.add(site);
+        site = new Site(2, "web02", "192.168.0.2", "搜狐", "www.sohu.com", 200);
+        site.steps = Lists.newArrayList();
+        site.steps.add(new Step(3, "step01", "www.sohu.com/music", "", 4000, "结果01", 200));
+        site.steps.add(new Step(4, "step02", "www.sohu.com/games", "", 5000, "结果02", 200));
+        all.add(site);
+        AjaxPageableResponse<Site> response = new AjaxPageableResponse<>();
+        response.setList(all);
+        return response;
+    }
+
+    class Site {
+        public long id;
+        public String host;
+        public String ip;
+        public String web;
+        public String url;
+        public int status;
+        public List<Step> steps;
+
+        public Site() {
+        }
+
+        public Site(long id, String host, String ip, String web, String url, int status) {
+            this.id = id;
+            this.host = host;
+            this.ip = ip;
+            this.web = web;
+            this.url = url;
+            this.status = status;
+        }
+    }
+
+    class Step {
+        public Step() {
+        }
+
+        public Step(long id, String name, String url, String post, int timeout, String result, int code) {
+            this.id = id;
+            this.name = name;
+            this.url = url;
+            this.post = post;
+            this.timeout = timeout;
+            this.result = result;
+            this.code = code;
+        }
+
+        public long id;
+        public String name;
+        public String url;
+        public String post;
+        public int timeout;
+        public String result;
+        public int code;
+    }
 }
